a3fc0af4130d5542efe5d721fb2157c8a95d854e
Jake <32019706+JakaPresecnik@users.noreply.github.com>
2018-06-12 00:58:58 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ $('.nanodegree').mouseleave(function() {
   $('.nanodegree img').removeClass('reveal');
   $('.nanodegree figcaption').addClass('hidden');
 })
+$('.certificate').mouseover(function() {
+  $(this).children('p').removeClass('hidden');
+  $(this).children('img').css('width', '50%');
+  $(this).children('img').css('margin', '0px auto');
+  $(this).parents('.boxes').addClass('box_small_bottom');
+})
+$('.certificate').mouseleave(function() {
+  $(this).children('img').css('width', '80%');
+  $(this).children('img').css('margin', '10px auto');
+  $(this).children('p').addClass('hidden');
+  $(this).parents('.boxes').removeClass('box_small_bottom');
+})
